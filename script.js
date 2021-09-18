97ef53e3f9d08c002c02d059062295ec2f721025
acliboro <acliboro@gmail.com>
2021-09-19 01:20:56 +0800

    First commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var slider = document.getElementById("rate");
+var output = document.getElementById("ratex");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
 function compute()
 {
     p = document.getElementById("principal").value;
