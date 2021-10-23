d62155bec0d4f4edff65fdf16a971042d512db61
amiel <amiel.liboro@recordedfuture.com>
2021-10-23 22:01:12 +0800

    fixed form behavior

--- script.js
+++ script.js
@@ -2,10 +2,22 @@
 function updateTextInput(val){
   document.getElementById('ratex').innerHTML=val;
 }
-    
-function compute()
-{   
-    event.preventDefault();
+
+var calcform = document.getElementById("calculatorform");
+if (calcform) {
+  calcform.addEventListener("submit", function(e) {
+    e.preventDefault()
+  });
+}
+
+window.onload = function() { 
+  document.getElementById("calculatorform").onsubmit = function() { 
+      compute();
+      return false;
+  };
+};
+
+function compute() {   
     const d = new Date()
     let year_today = d.getFullYear();
     var result_node = document.getElementById("result");
@@ -23,7 +35,5 @@ function compute()
     var fourthline = "in the year <mark>" + total_year + "</mark>"
     var totaline = "<p>"+firstline+secondline+thirdline+fourthline+"</p>"
     result_node.insertAdjacentHTML('afterbegin', totaline)
-    
-      
 }
         
